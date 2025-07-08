feea20203aef86243cc32b9583b504e4e31cd9e7
orcamania5 <jmoh78w4@hotmail.com>
2025-07-08 21:52:35 +0900

    Add files via upload

---
+++ 소스0708.cpp
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("æ»≥Á«œººø‰\n");
+//	printf("æ»≥Á«œººø‰");
+//	printf("æ»≥Á«œººø‰\n");
+//	return 0;
+//}
+#include<stdio.h>
+
+int main()
+{
+	double pi;
+	pi = 3.14;
+	float e = 2.718;
+	printf("pi=%f, e=%lf.. \n", pi, e);
+	return 0;
+}
