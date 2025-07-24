c664268c051cd63841175c9453746d54c8c56142
orcamania5 <jmoh78w4@hotmail.com>
2025-07-24 19:17:16 +0900

    Add files via upload

--- 소스0708.cpp
+++ 소스0708.cpp
@@ -1138,10 +1138,13 @@
 //	return 0;
 //}
 
+////[Starting Codes]
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
+#include<math.h>
 
 //[~페이지32 문제2~]
 //int main()
@@ -1711,25 +1714,310 @@
 //}
 
 
-//[~페이지39 문제1~]
-int main()
-{
-	int product;
-	int number;
-	int multipler;
-	return 0;
-}
+////[~페이지39 문제1~]
+//int main()
+//{
+//	int product;
+//	int number;
+//	int multiplier;
+//
+//	for (number = 1; number < 10; number++)
+//	{
+//		for (multiplier = 1; multiplier < 10; multiplier++)
+//		{
+//			product = number * multiplier;
+//			printf("%d X %d = %d\n", number, multiplier, product);
+//		}
+//	}
+//	return 0;
+//}
 
 ////[~페이지39 문제2~]
 //int main()
 //{
+//	int product;
+//	int number;
+//	int multiplier;
 //
+//	for (number = 1; number < 10; number++)
+//	{
+//		if (number == 3)
+//		{
+//			continue;
+//		}
+//		for (multiplier = 1; multiplier < 10; multiplier++)
+//		{
+//			product = number * multiplier;
+//			printf("%d * %d = %d\n", number, multiplier, product);
+//		}
+//	}
 //	return 0;
 //}
 //
 ////[~페이지39 문제3~]
 //int main()
 //{
+//	int dice4, dice6;
+//
+//	for (dice4 = 1; dice4 <= 4; dice4++)
+//	{
+//		for (dice6 = 1; dice6 <= 6; dice6++)
+//		{
+//			printf("4면 - %d, 6면 - %d\n", dice4, dice6);
+//		}
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int outer;
+//	int inter;
+//	for (outer = 0; outer < 5; outer++)
+//	{
+//		printf("%d번째 내부 반복문 진입 \n", outer);
+//
+//		for (inter = 0; inter < 5; inter++)
+//		{
+//			printf("내부 반복문 %d번 반복\n", inter);
+//		}
+//		printf("%d번째 내부 반복문 탈출 \n \n", outer);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int outer;
+//	int inter;
+//	for (outer = 0; outer < 5; outer++)
+//	{
+//		for (inter = 0; inter < 5; inter++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int outer;
+//	int inter;
+//	for (outer = 0; outer < 5; outer++)
+//	{
+//		for (inter = 0; inter < outer + 1; inter++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int outer, inter1, inter2;
+//	for (outer = 0; outer < 5; outer++)
+//	{
+//		for (inter1 = 0; inter1 < 5 - outer; inter1++)
+//		{
+//			printf(" ");
+//		}
+//		for (inter2 = 0; inter2 < outer + 1; inter2++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int outer;
+//	int inter1, inter2;
+//	for (outer = 0; outer < 5; outer++)
+//	{
+//		for (inter1 = 0; inter1 < 5 - outer; inter1++)
+//		{
+//			printf(" ");
+//		}
+//		for (inter2 = 0; inter2 < outer + 1; inter2++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+////[~부가 연습 페이지들 (시작)~]
+//int main()
+//{
+//	printf("*");
+//	printf("\n");
+//	printf("*");
+//	printf("*");
+//	printf("\n");
+//	printf("*");
+//	printf("*");
+//	printf("*");
+//	printf("\n");
 //
 //	return 0;
+//}
+
+//int main()
+//{
+//	for (int x = 1; x <= 1; x++)
+//	{
+//		printf("*");
+//	}
+//	printf("\n");
+//	for (int x = 1; x <= 2; x++)
+//	{
+//		printf("*");
+//	}
+//	printf("\n");
+//	for (int x = 1; x <= 3; x++)
+//	{
+//		printf("*");
+//	}
+//	printf("\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int y = 1;
+//	for (int x = 1; x <= y; x++)
+//	{
+//		printf("*");
+//	}
+//	printf("\n");
+//	y += 1;
+//
+//	for (int x = 1; x <= y; x++)
+//	{
+//		printf("*");
+//	}
+//	printf("\n");
+//	y += 1;
+//
+//	for (int x = 1; x <= y; x++)
+//	{
+//		printf("*");
+//	}
+//	printf("\n");
+//	y += 1;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	for (int y = 1; y <= 3; y +=1)
+//	{
+//		for (int x = 1; x <= y; x++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int LINE = 3;
+//	for (int y = 1; y <= LINE; y += 1)
+//	{
+//		for (int x = 1; x <= y; x++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int LINE = 3;
+//	scanf("%d", &LINE);
+//	for (int y = 1; y <= LINE; y += 1)
+//	{
+//		for (int x = 1; x <= y; x++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+////[~부가 연습 페이지들 (끝)~]
+
+////[~페이지41 문제1~]
+//int main()
+//{
+//
+//	return 0;
+//}
+
+//[~페이지41 문제2~]
+//int main()
+//{
+//
+//	return 0;
+//}
+
+//[~페이지41 문제3~]
+//int main()
+//{
+//
+//	return 0;
+//}
+
+//[~페이지41 문제4~]
+//int main()
+//{
+//
+//	return 0;
+//}
+
+//[~페이지41 문제5~]
+//int main()
+//{
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char str[100];
+//	int str_cnt;
+//	int i;
+//
+//	printf("영문자 및 숫자를 입력 (100자 이하) : ");
+//	scanf("%s", str);
+//
+//	printf("\n");
+//	printf("입력한 문자열 ==> %s\n", str);
+//	printf("변환된 문자열 ==>");
+//
+//	str_cnt = strlen(str);
+//
+//	for (str_cnt; str_cnt > 0; str_cnt--)
+//	{
+//		if()
+//	}
+//
+//	printf("\n");
 //}
