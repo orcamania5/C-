2f702061406108e5a43039eb8750cf73964e934a
orcamania5 <jmoh78w4@hotmail.com>
2025-07-31 21:56:55 +0900

    Add files via upload

--- 소스0708.cpp
+++ 소스0708.cpp
@@ -3232,7 +3232,392 @@
 //	return 0;
 //}
 
-void person_A()
-{
-	int money = 10000;
-}
+//void person_A()
+//{
+//	int money = 10000;
+//	printf("A: 주머니에");
+//	printf("%d원\n", money);
+//}
+//
+//void person_B()
+//{
+//	int money = 5000;
+//	printf("B: 주머니에");
+//	printf("%d원\n", money);
+//}
+//int main()
+//{
+//	person_A();
+//	person_B();
+//
+//	return 0;
+//}
+
+//int function_test2()
+//{
+//	int testNumber = 5;
+//	return testNumber;
+//}
+//int main()
+//{
+//	int getNumber;
+//	getNumber = function_test2();
+//	printf("function_test2()");
+//	printf("에서 가져온 값 : ");
+//	printf("%d\n", getNumber);
+//}
+
+//void sayHi()
+//{
+//	printf("hi");
+//	return;
+//}
+//char getA()
+//{
+//	return 'A';
+//}
+//int get5()
+//{
+//	return 5;
+//}
+//double set1get5(int one)
+//{
+//	return one + 4.0;
+//}
+//
+//void main()
+//{
+//	sayHi();
+//	printf("%c\n", getA());
+//	printf("%d\n", get5());
+//	printf("%f\n", set1get5(1));
+//}
+
+//void sayHi();
+//char getA();
+//int get5();
+//double set1get5(int one);
+
+//void main()
+//{
+//	sayHi();
+//	printf("%c\n", getA());
+//	printf("%d\n", get5());
+//	printf("%f\n", set1get5(1));
+//}
+//void sayHi()
+//{
+//	printf("hi");
+//	return;
+//}
+//char getA()
+//{
+//	return 'A';
+//}
+//int get5()
+//{
+//	return 5;
+//}
+//double set1get5(int one)
+//{
+//	return one + 4.0;
+//}
+
+//float sendCard();
+//int main()
+//{
+//	float result = sendCard();
+//	printf("%f만원 받음\n", result);
+//	return 0;
+//}
+//float sendCard()
+//{
+//	printf("<크리스마스 카드>\n");
+//	printf("비용: 0.7만원\n");
+//	printf("돈내기(단위:만원) : ");
+//	int manwon;
+//	scanf("%d", &manwon);
+//	float change = manwon - 0.7;
+//	return change;
+//}
+
+//char getA() { return 'A'; }
+//char getB() { return 'B'; }
+//char getE() { return 'E'; }
+//char getF() { return 'F'; }
+//char getL() { return 'L'; }
+//char getG() { return 'G'; }
+//char getP() { return 'P'; }
+//int main()
+//{
+//	printf("벌을 영어로?\n");
+//	printf("%c%c%c\n", getB(), getE(), getE());
+//	return 0;
+//}
+
+////[~페이지10 문제1~]
+//char getA() { return 'A'; }
+//char getB() { return 'B'; }
+//char getE() { return 'E'; }
+//char getF() { return 'F'; }
+//char getL() { return 'L'; }
+//char getG() { return 'G'; }
+//char getP() { return 'P'; }
+//int main()
+//{
+//	printf("사과을 영어로?\n");
+//	printf("%c%c%c%c%c\n", getA(), getP(), getP(), getL(), getE());
+//	return 0;
+//}
+
+////[~페이지10 문제2~]
+//char getA() { return 'A'; }
+//char getB() { return 'B'; }
+//char getE() { return 'E'; }
+//char getF() { return 'F'; }
+//char getL() { return 'L'; }
+//char getG() { return 'G'; }
+//char getP() { return 'P'; }
+//char getT() { return 'T'; }
+//int main()
+//{
+//	printf("사과을 영어로?\n");
+//	printf("%c%c%c%c\n", getL(), getE(), getF(), getT());
+//	return 0;
+//}
+
+//void makeHamburger(int count)
+//{
+//	printf("햄버거 %d개 나왔습니다.\n", count);
+//}
+//int main()
+//{
+//	int ham_count;
+//	printf("햄버거 몇 개 주문하시겠습니까?\n");
+//	scanf("%d", &ham_count);
+//	makeHamburger(ham_count);
+//	return 0;
+//}
+
+//void print_Number();
+//void print_Number2(int num);
+//void print_Number4_charB(int num, char b);
+//void print_characterX(char X);
+//
+//int main()
+//{
+//	print_Number();
+//	print_Number2(2);
+//	print_Number4_charB(4, 'b');
+//	print_characterX('X');
+//	return 0;
+//}
+//void print_Number()
+//{
+//	printf("Number\n");
+//}
+//void print_Number2(int num)
+//{
+//	printf("Number : %d\n", num);
+//}
+//void print_Number4_charB(int num, char b)
+//{
+//	printf("Number : %d.character : %c\n", num, b);
+//}
+//void print_characterX(char X)
+//{
+//	printf("character : %c\n", X);
+//}
+
+//char rightType(char type)
+//{
+//	if (type != 'A' && type != 'B')
+//	{
+//		printf("주문할 수 없는 햄버거입니다.\n");
+//		return 'C';
+//	}
+//	return type;
+//}
+//void makeHamburger(char type, int count)
+//{
+//	if (type == 'A')
+//	{
+//		printf("A타입 햄버거");
+//	}
+//	else
+//	{
+//		printf("B타입 햄버거");
+//	}
+//	printf(" %d개 나왔습니다.\n", count);
+//}
+//int main()
+//{
+//	char ham_type;
+//	int ham_count;
+//	printf("어느타입 햄버거를");
+//	printf("주문하시겠습니까?(A or B)\n");
+//	scanf("%c", &ham_type);
+//	if (rightType(ham_type) == 'C')
+//	{
+//		return 0;
+//	}
+//	printf("햄버거 몇 개 주문하시겠습니까?\n");
+//	scanf("%d", &ham_count);
+//	makeHamburger(ham_type, ham_count);
+//	return 0;
+//}
+
+//int add_number(int num1, int num2)
+//{
+//	int retVal = num1 + num2;
+//	return retVal;
+//}
+//int main()
+//{
+//	int a = 1, b = 2;
+//	int data1 = add_number(a, b);
+//	printf("결과 : %d\n", data1);
+//	return 0;
+//}
+
+////[~페이지12 문제1~]
+//int add_number(int num1, int num2)
+//{
+//	int retVal = num1 + num2;
+//	return retVal;
+//}
+//int subtract_number(int num1, int num2)
+//{
+//	int retVal = num1 - num2;
+//	return retVal;
+//}
+//int multiply_number(int num1, int num2)
+//{
+//	int retVal = num1 * num2;
+//	return retVal;
+//}
+//float divide_number(int num1, int num2)
+//{
+//	float retVal = num1 / (float)num2;
+//	return retVal;
+//}
+//int main()
+//{
+//	int a = 1, b = 2;
+//	int data1 = add_number(a, b);
+//	printf("더하기 결과 : %d\n", data1);
+//	int data2 = subtract_number(a, b);
+//	printf("빼기 결과 : %d\n", data2);
+//	int data3 = multiply_number(a, b);
+//	printf("곱하기 결과 : %d\n", data3);
+//	float data4 = divide_number(a, b);
+//	printf("나누기 결과 : %f\n", data4);
+//	return 0;
+//}
+
+//재귀호출에 관한 문제
+//int minus(int n)
+//{
+//	if (n <= 1) return -3;
+//	return minus(n - 1) - 2;
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("%d", minus(n));
+//	return 0;
+//}
+
+//void printNumber(int n)
+//{
+//	if (n <= 0) return;
+//	printf("%d", n);
+//	printNumber(n - 1);
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printNumber(n);
+//	return 0;
+//}
+
+//int fibonacci(int n)
+//{
+//	printf("%d", n);
+//	if (n <= 2) return 1;
+//	printf("%d\n", n);
+//	return fibonacci(n - 1) + fibonacci(n - 2);
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("\n%d\n", fibonacci(n));
+//	return 0;
+//}
+
+////[~페이지13 문제1~]
+//int keep_adding(int n)
+//{
+//	if (n == 1) return 1;
+//	return n + keep_adding(n - 1);
+//}
+//int main()
+//{
+//	int num;
+//	scanf("%d", &num);
+//	printf("%d\n", keep_adding(num));
+//	return 0;
+//}
+
+//struct BYTE
+//{
+//	char a;
+//	char b;
+//};
+//
+//void main()
+//{
+//	struct BYTE a;
+//	a.a = 10;
+//	a.b = 20;
+//	printf("%d %d", a.a, a.b);
+//}
+
+//struct student
+//{
+//	int id;
+//	char* name;
+//	float aver;
+//};
+//
+//void main()
+//{
+//	struct student s = { 1, (char*)"이 샘", 90.5 };
+//	printf("아이디: %d\n", s.id);
+//	printf("이름: %s\n", s.name);
+//	printf("백분율: %lf\n", s.aver);
+//}
+
+//struct GUN
+//{
+//	char name[10];
+//	float bullet_type;
+//	bool scope;
+//	bool muzzle;
+//	bool handgrip;
+//	bool megazine;
+//	bool tactical_stock;
+//	int magazine_size;
+//};
+//
+//int main()
+//{
+//	struct GUN M416 = {"M416", 5.56, true, true, true, true, true, 30};
+//	printf("화기명: %s\n", M416.name);
+//	printf("탄 종류: %.2f\n", M416.bullet_type);
+//	printf("스코프유무: %s\n", M416.scope ? "true":"false");
+//	return 0;
+//}
